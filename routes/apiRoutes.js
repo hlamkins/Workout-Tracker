6b89fe43221b72c784b0a92680bc2a81bd6b5e48
Heather Lamkins <lamkinsheather@gmail.com>
2021-02-06 09:20:20 -0600

    added api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,77 @@
+const Workout = require("../models/workout.js");
+
+module.exports = function (app) {
+    
+    app.get("/api/workouts", function (req, res) {
+        Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: "$exercises.duration",
+                    },
+                },
+            },
+        ])
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/range", function (req, res) {
+        Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: "$exercises.duration",
+                    },
+                },
+            },
+        ])
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts/range", function (req, res) {
+        Workout.create({})
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts/range", function (req, res) {
+        Workout.create({})
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+    });
+
+    app.put("/api/workouts/:id", function (req, res) {
+        Workout.findByIdAndUpdate(
+            req.params.id,
+            {
+                $push: { exercises: req.body },
+            },
+            { new: true, runValidators: true }
+        )
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+    });
+
+};
