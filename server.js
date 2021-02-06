73b0180de30fd3b2eb79da2c3e9ffe378ab95005
Heather Lamkins <lamkinsheather@gmail.com>
2021-02-06 09:05:12 -0600

    Update server.js with needed paths

--- server.js
+++ server.js
@@ -5,3 +5,25 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(logger("dev"));
+
+app.use(express.static('public'));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
+app.listen(PORT, () => {
+    console.log('App is running on port ${PORT}!');
+});
